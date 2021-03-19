584c7a40fd91cef5275c0d2af60a405d6169024b
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-19 09:51:24 -0400

    tasks table completed in migrations

--- data/migrations/20210319132846_first-migration.js
+++ data/migrations/20210319132846_first-migration.js
@@ -1,22 +1,28 @@
-
 exports.up = function (knex) {
   return knex.schema
-  .createTable('projects', (tbl) => {
-    tbl.increments('project_id');
-    tbl.string('project_name', 128).notNullable();
-    tbl.text('project_description');
-    tbl.boolean('project_completed').setdefault(false);
-  })
-  .createTable('resources', (tbl) => {
-    tbl.increments('resource_id');
-    tbl.string('resource_name', 128).notNullable().unique()
-    tbl.text('resource_description');
-  });
-  
-  
-
+    .createTable('projects', (tbl) => {
+      tbl.increments('project_id');
+      tbl.string('project_name', 128).notNullable();
+      tbl.text('project_description');
+      tbl.boolean('project_completed').setdefault(false);
+    })
+    .createTable('resources', (tbl) => {
+      tbl.increments('resource_id');
+      tbl.string('resource_name', 128).notNullable().unique();
+      tbl.text('resource_description');
+    })
+    .createTable('tasks', (tbl) => {
+      tbl.increments('task_id');
+      tbl.string('task_name', 128).notNullable();
+      tbl.text('task_description');
+      tbl.boolean('task_completed').setdefault(false);
+      tbl
+        .integer('project_id')
+        .usigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects');
+    });
 };
 
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {};
