765da35364763da93a2afb8b04ee2101dfe580c7
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-19 14:01:12 -0400

    created and completed resource model

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,43 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig');
+
+function get() {
+  return db('resources');
+}
+
+function getById(id) {
+  return db('resources').where('resource_id', id).first();
+}
+
+function create(resource) {
+  return db('resources')
+    .insert(resource)
+    .then((ids) => {
+      return getById(ids[0]);
+    });
+}
+
+function update(id, resource) {
+  return db('resources')
+    .where('resource_id', id)
+    .update(resource)
+    .then(() => {
+      return db('resources').where('resource_id', id).first();
+    });
+}
+
+function remove(id) {
+    return db('resources')
+    .where('resource_id', id)
+    .del()
+    .then(() => {
+        return db('resources')
+    })
+}
+
+module.exports = {
+  get,
+  getById,
+  create,
+  update,
+  remove,
+};
