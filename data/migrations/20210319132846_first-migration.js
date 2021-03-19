a6953549eb393ae574fb389a61b485f230c38dee
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-19 10:02:58 -0400

    typo fixed setdefault to setDefault threw error upon trying to migrate to latest

--- data/migrations/20210319132846_first-migration.js
+++ data/migrations/20210319132846_first-migration.js
@@ -4,7 +4,7 @@ exports.up = function (knex) {
       tbl.increments('project_id');
       tbl.string('project_name', 128).notNullable();
       tbl.text('project_description');
-      tbl.boolean('project_completed').setdefault(false);
+      tbl.boolean('project_completed').setDefault(false);
     })
     .createTable('resources', (tbl) => {
       tbl.increments('resource_id');
@@ -15,7 +15,7 @@ exports.up = function (knex) {
       tbl.increments('task_id');
       tbl.string('task_name', 128).notNullable();
       tbl.text('task_description');
-      tbl.boolean('task_completed').setdefault(false);
+      tbl.boolean('task_completed').setDefault(false);
       tbl
         .integer('project_id')
         .usigned()
