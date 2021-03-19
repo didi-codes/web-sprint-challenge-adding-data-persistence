ac4f06ddafb5e1bda490078da8cdccbdb7e472e6
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-19 11:00:31 -0400

    create and exported server

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
-// build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+server.get('*', (req, res) => {
+  res.status(200).json({
+    message: 'Welcome To Lambda Projects API',
+  });
+});
+
+module.exports = server;
