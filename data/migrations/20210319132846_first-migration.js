71de45eaee3b238c42685b18c0f1272971d1e47f
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-19 09:41:44 -0400

    created projects table in migrations folder

--- data/migrations/20210319132846_first-migration.js
+++ data/migrations/20210319132846_first-migration.js
@@ -1,6 +1,15 @@
 
-exports.up = function(knex) {
+exports.up = function (knex) {
+  return knex.schema
+  .createTable('projects', (tbl) => {
+    tbl.increments('project_id');
+    tbl.string('project_name', 128).notNullable();
+    tbl.text('project_description');
+    tbl.boolean('project_completed').setdefault(false);
+  });
   
+  
+
 };
 
 exports.down = function(knex) {
