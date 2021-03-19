7d9bf086707cf0563465eb384b09d7dd5ad63dca
Damaris Garcia <damaris-garcia@lambastudents.com>
2021-03-19 11:12:32 -0400

    created first model function, endpoint and exported project to the server, testing in postman

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,22 @@
-// build your `/api/projects` router here
+const express = require('express');
+const Project = require('./model');
+
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const data = await Project.get();
+    res.status(200).json(data);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const helmet = require('helmet');
 
+const projectRouter = require('./project/router');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.use('api/project', projectRouter);
 
 server.get('*', (req, res) => {
   res.status(200).json({
